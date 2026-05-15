54de845fce44bec3688db354ada3d634eea38af5
priyabratadas <pdas544@gmail.com>
2026-05-15 11:19:24 +0530

    project init with new route

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/HomePage.vue'
+import Basket from '../views/ShoppingBasket.vue'
+import Product from '../views/Product.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/basket',
+    name: 'Basket',
+    component: Basket
+  },
+  {
+    path: '/products',
+    name: 'Product',
+    component: Product
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/Product.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Product Page</h1>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    name: 'Product',
+  
+    methods: {
+     
+    },
+   
+  };
+
+</script>
